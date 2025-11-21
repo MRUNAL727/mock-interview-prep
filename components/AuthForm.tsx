a38c041e22ffefac0a0b5ea1662480324750b995
MRUNAL727 <mrunal4271@gmail.com>
2025-11-21 12:27:43 +0530

    sign-in & sign-up pages

---
+++ components/AuthForm.tsx
@@ -0,0 +1,137 @@
+"use client";
+import React from "react";
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "../components/ui/card";
+import Image from "next/image";
+import Link from "next/link";
+import { toast } from "sonner";
+import FormField from "./FormField";
+import { useRouter } from "next/navigation";
+
+const authFormaSchema = (type: FormType) => {
+  return z.object({
+    name: type === "sign-up" ? z.string().min(3) : z.string().optional(),
+    email: z.email(),
+    password: z.string().min(6),
+  });
+};
+
+const AuthForm = ({ type }: { type: FormType }) => {
+  const formSchema = authFormaSchema(type);
+  const router = useRouter();
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  function onSubmit(data: z.infer<typeof formSchema>) {
+    try {
+      if (type === "sign-up") {
+        toast.success("Account created successefully. Please sign in.");
+        router.push("/sign-in");
+      } else {
+        toast.success("Signed in successfully!");
+        router.push("/");
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(`There was an error: ${error}`);
+    }
+  }
+
+  const isSignIn = type === "sign-in";
+
+  return (
+    <div className="card-border lg:min-width-[566px]">
+      <div className="flex flex-col gap-6 card py-14 px-10">
+        <div className="flex flex-row gap-2 justify-center">
+          <Image src="/logo.svg" alt="Logo" width={32} height={38} />
+          <h2 className="text-primary-100">PrepWise</h2>
+        </div>
+        <h3>Practice job interview with AI</h3>
+        <Card className="w-full sm:max-w-md">
+          <CardHeader>
+            <CardTitle>Profile Settings</CardTitle>
+            <CardDescription>
+              Update your profile information below.
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <form
+              className="form w-full space-y-6 mt-4"
+              id="form-rhf-input"
+              onSubmit={form.handleSubmit(onSubmit)}
+            >
+              {!isSignIn && (
+                <FormField
+                  control={form.control}
+                  name="name"
+                  label="Name"
+                  placeholder="Name"
+                  type="text"
+                />
+              )}
+              <FormField
+                control={form.control}
+                name="email"
+                label="Email"
+                placeholder="Email Addresss"
+                type="email"
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                label="Password"
+                placeholder="Password"
+                type="password"
+              />
+
+              <Button className="btn" type="submit">
+                {isSignIn ? "Sign In" : "Create an account"}
+              </Button>
+            </form>
+          </CardContent>
+          <p className="text-center">
+            {isSignIn ? "Don't have an account?" : "Already have an account?"}
+            <Link
+              href={!isSignIn ? "/sign-in" : "/sign-up"}
+              className="font-bold text-user-primary ml-1"
+            >
+              {!isSignIn ? "Sign In" : "Sign Up"}
+            </Link>
+          </p>
+          {/* <CardFooter>
+          <Field orientation="horizontal">
+            <Button
+              type="button"
+              variant="outline"
+              onClick={() => form.reset()}
+            >
+              Reset
+            </Button>
+            <Button type="submit" form="form-rhf-input">
+              Save
+            </Button>
+          </Field>
+        </CardFooter> */}
+        </Card>
+      </div>
+    </div>
+  );
+};
+
+export default AuthForm;
---
+++ components/FormField.tsx
@@ -0,0 +1,56 @@
+"use client";
+import React from "react";
+import { Field, FieldError, FieldGroup, FieldLabel } from "./ui/field";
+import { Control, Controller, FieldValues, Path } from "react-hook-form";
+import { Input } from "./ui/input";
+
+interface FormFieldProps<T extends FieldValues> {
+  control: Control<T>;
+  name: Path<T>;
+  label: string;
+  placeholder?: string;
+  type?: "text" | "email" | "password" | "file";
+}
+
+const FormField = ({
+  control,
+  name,
+  label,
+  placeholder,
+  type = "text",
+}: FormFieldProps<T>) => {
+  return (
+    <div>
+      <FieldGroup>
+        <Controller
+          name={name}
+          control={control}
+          render={({ field, fieldState }) => (
+            <Field data-invalid={fieldState.invalid}>
+              <FieldLabel htmlFor={name} className="label">
+                {label}
+              </FieldLabel>
+              <Input
+                {...field}
+                className="input"
+                id={name}
+                aria-invalid={fieldState.invalid}
+                placeholder={placeholder}
+                autoComplete="username"
+                type={type}
+              />
+              {/* <FieldDescription>
+                    This is your public display name. Must be between 3 and 10
+                    characters. Must only contain letters, numbers, and
+                    underscores.
+                  </FieldDescription> */}
+              {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+            </Field>
+          )}
+        />
+      </FieldGroup>
+    </div>
+  );
+};
+
+export default FormField;
